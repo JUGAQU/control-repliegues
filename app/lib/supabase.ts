cd5fd46da9fc91cc999c83aa1ba8db672755f0d3
JUGAQU <73647480+JUGAQU@users.noreply.github.com>
2026-04-19 20:44:06 +0200

    Update supabase.ts

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://sflfymzqhptzdmkevkxw.supabase.co";
-const supabaseKey = "sb_publishable_oEuaqbpg1Ff_9RJ1SGDgfQ_lzAPlM0Q";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
