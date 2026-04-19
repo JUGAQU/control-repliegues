c0a16bfe9527bf1d62a69b57fdeb85722396379c
JUGAQU <73647480+JUGAQU@users.noreply.github.com>
2026-04-19 12:58:33 +0200

    Update page.tsx

--- app/listado/page.tsx
+++ app/listado/page.tsx
@@ -71,17 +71,19 @@ setOrden({ campo, direccion });
 const datosFiltrados = datos
   .filter((item) => {
     return (
-      (item.atlas ?? "").toString().toLowerCase().includes(filtros.atlas.toLowerCase()) &&
-      (item.lote ?? "").toString().toLowerCase().includes(filtros.lote.toLowerCase()) &&
-      (item.nombre ?? "").toString().toLowerCase().includes(filtros.nombre.toLowerCase()) &&
-      (item.provincia ?? "").toString().toLowerCase().includes(filtros.provincia.toLowerCase()) &&
-      (item.miga ?? "").toString().toLowerCase().includes(filtros.miga.toLowerCase()) &&
-      (item.coordenadas ?? "").toString().toLowerCase().includes(filtros.coordenadas.toLowerCase()) &&
-      (item.tipo_edificio ?? "").toString().toLowerCase().includes(filtros.tipo_edificio.toLowerCase()) &&
-      (item.tipo_repliegue ?? "").toString().toLowerCase().includes(filtros.tipo_repliegue.toLowerCase()) &&
-      (item.tipo_senda ?? "ACELERADA_2026").toString().toLowerCase().includes(filtros.tipo_senda.toLowerCase()) &&
-      (item.fecha_abandono ?? "").toString().toLowerCase().includes(filtros.fecha_abandono.toLowerCase()) &&
-      (item.prioritario ? "si" : "no").includes(filtros.prioritario.toLowerCase())
+      (item.atlas ?? "").toString().toLowerCase().includes((filtros.atlas ?? "").toLowerCase()) &&
+      (item.lote ?? "").toString().toLowerCase().includes((filtros.lote ?? "").toLowerCase()) &&
+      (item.nombre ?? "").toString().toLowerCase().includes((filtros.nombre ?? "").toLowerCase()) &&
+      (item.provincia ?? "").toString().toLowerCase().includes((filtros.provincia ?? "").toLowerCase()) &&
+      (item.miga ?? "").toString().toLowerCase().includes((filtros.miga ?? "").toLowerCase()) &&
+      (item.coordenadas ?? "").toString().toLowerCase().includes((filtros.coordenadas ?? "").toLowerCase()) &&
+      (item.tipo_edificio ?? "").toString().toLowerCase().includes((filtros.tipo_edificio ?? "").toLowerCase()) &&
+      (item.tipo_repliegue ?? "").toString().toLowerCase().includes((filtros.tipo_repliegue ?? "").toLowerCase()) &&
+      (item.tipo_senda ?? "ACELERADA_2026").toString().toLowerCase().includes((filtros.tipo_senda ?? "").toLowerCase()) &&
+      (item.fecha_abandono ?? "").toString().toLowerCase().includes((filtros.fecha_abandono ?? "").toLowerCase()) &&
+      (item.prioritario ? "si" : "no").includes(
+        (filtros.prioritario ?? "").toLowerCase()
+      )
     );
   })
 .sort((a: any, b: any) => {
