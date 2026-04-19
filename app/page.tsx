2f3a52ecdd14207428b119d020a84cd3a9d5b97f
JUGAQU <73647480+JUGAQU@users.noreply.github.com>
2026-04-19 20:32:52 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,44 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Home() {
-  return <div>Home OK</div>
+  const router = useRouter();
+
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 30,
+        background: "#f5f5f5",
+      }}
+    >
+      {/* LOGO */}
+      <img
+        src="/logo.png"
+        alt="Logo"
+        style={{ maxWidth: 300 }}
+      />
+
+      {/* BOTÓN */}
+      <button
+        onClick={() => router.push("/listado")}
+        style={{
+          padding: "12px 30px",
+          fontSize: 16,
+          background: "#0070f3",
+          color: "white",
+          border: "none",
+          borderRadius: 5,
+          cursor: "pointer",
+        }}
+      >
+        Entrar
+      </button>
+    </div>
+  );
 }
