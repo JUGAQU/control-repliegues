af01f39ea65687de6231329b74dca3533f75dcd6
JUGAQU <73647480+JUGAQU@users.noreply.github.com>
2026-04-21 10:52:12 +0200

    Update page.tsx

--- app/ficha/page.tsx
+++ app/ficha/page.tsx
@@ -231,7 +231,7 @@ export default function Ficha() {
       >
         <div style={campo}>
           <span>Proyecto Inversión:</span>
-          <input name="proyecto_inversio" value={formData.proyecto_inversion || ""} style={{ ...valor, width: 10, }}
+          <input name="proyecto_inversio" value={formData.proyecto_inversion || ""} style={{ ...valor, width: 80, }}
           />
         </div>
    
