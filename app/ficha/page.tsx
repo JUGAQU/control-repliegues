9d001cdc980076d9ac836d265cc915de7d6792ad
JUGAQU <73647480+JUGAQU@users.noreply.github.com>
2026-04-28 14:05:46 +0200

    Update page.tsx

--- app/ficha/page.tsx
+++ app/ficha/page.tsx
@@ -1358,7 +1358,7 @@ observaciones_preparacion_reasignacion:
                 <CampoInputAuto
                   label="Observaciones Preparación"
                   value={r.observaciones_preparacion_reasignacion || ""}
-                  minWidth={900}
+                  minWidth={1100}
                   onChange={(v)=>handleReasignacionChange(index,"observaciones_preparacion_reasignacion",v)}
                 />
               </div>
