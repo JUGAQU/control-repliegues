d928fcd3eaf449ef824a55341ca7bcda4a6d2476
JUGAQU <73647480+JUGAQU@users.noreply.github.com>
2026-04-28 13:33:39 +0200

    Update page.tsx

--- app/ficha/page.tsx
+++ app/ficha/page.tsx
@@ -1171,9 +1171,9 @@ observaciones_preparacion_reasignacion:
               <CampoReaSoloLecturaAuto label="Tipo Diversificado" value={r.tipo_diversificado} minWidth={100}/>
               <CampoReaSoloLecturaAuto label="Tipo Interface" value={r.tipo_velocidad_interface} minWidth={100}/>
               <CampoReaSoloLecturaAuto label="Veloc. Interface" value={r.velocidad_interface} minWidth={60}/>
-              <CampoInputAuto label="Prueba de Atenuación" value={r.pba_atenuacion || ""} minWidth={100} onChange={(v)=>handleReasignacionChange(index,"pba_atenuacion",v)}/>
+              <CampoInputAuto label="Prueba de Atenuación" value={r.pba_atenuacion || ""} minWidth={130} onChange={(v)=>handleReasignacionChange(index,"pba_atenuacion",v)}/>
               <CampoInputAuto label="Autonegociación" value={r.autonegociacion || ""} minWidth={100} onChange={(v)=>handleReasignacionChange(index,"autonegociacion",v)}/>
-              <CampoInputAuto label="Configuracion Puerto Destino" value={r.configuracion_puerto_destino || ""} minWidth={100} onChange={(v)=>handleReasignacionChange(index,"configuracion_puerto_destino",v)}/>
+              <CampoInputAuto label="Configuracion Puerto Destino" value={r.configuracion_puerto_destino || ""} minWidth={150} onChange={(v)=>handleReasignacionChange(index,"configuracion_puerto_destino",v)}/>
               <CampoInputAuto label="Ventana GECO" value={r.ventana_geco || ""} minWidth={100} onChange={(v)=>handleReasignacionChange(index,"ventana_geco",v)}/>
               
 
