f29041da04eede7e0a52472c8c97ab6b64f39c4f
JUGAQU <73647480+JUGAQU@users.noreply.github.com>
2026-04-29 16:35:50 +0200

    Update page.tsx

--- app/ficha/page.tsx
+++ app/ficha/page.tsx
@@ -504,6 +504,18 @@ observaciones_preparacion_reasignacion:
     (r:any) => grupoModoReasignacion(r.modo_reasignacion) === grupo
   ).length;
 
+  const gruposOrdenados = Object.entries(serviciosAgrupados).sort(
+    ([tituloA], [tituloB]) => {
+      const orden = (titulo: string) => {
+        if (titulo.startsWith("SGIPE")) return 1;
+        if (titulo.startsWith("Grupo")) return 2;
+        return 3;
+      };
+  
+      return orden(tituloA) - orden(tituloB);
+    }
+  );
+
 
   const serviciosAgrupados = reasignacionesEjecucionFiltradas.reduce(
   (acc:any, r:any) => {
