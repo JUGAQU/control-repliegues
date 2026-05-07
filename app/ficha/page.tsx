8451f607900fdaaeffd33a832079a2b7bbf25c5b
JUGAQU <73647480+JUGAQU@users.noreply.github.com>
2026-05-07 17:52:43 +0200

    Update page.tsx

--- app/ficha/page.tsx
+++ app/ficha/page.tsx
@@ -1442,7 +1442,7 @@ if (erroresActuaciones.length > 0) {
       }}
       title="Eliminar actuación"
     >
-      🗑️
+      ✖
     </button>
 
 
@@ -1465,7 +1465,7 @@ if (erroresActuaciones.length > 0) {
     }}
     title="Nueva actuación"
   >
-    +
+   ➕
   </button>
 )}
 
