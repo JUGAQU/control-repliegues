f311b21be58341c92b3b2a70d03b170bc0ade1a2
JUGAQU <73647480+JUGAQU@users.noreply.github.com>
2026-05-11 11:42:38 +0200

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,11 +4,12 @@ import AzureADProvider from "next-auth/providers/azure-ad";
 const handler = NextAuth({
   providers: [
     AzureADProvider({
-      clientId: "TU_CLIENT_ID",
-      clientSecret: "TU_SECRET",
-      tenantId: "TU_TENANT_ID",
+      clientId: process.env.AZURE_AD_CLIENT_ID!,
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET!,
+      tenantId: process.env.AZURE_AD_TENANT_ID!,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
 });
 
 export { handler as GET, handler as POST };
